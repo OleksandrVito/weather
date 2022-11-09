82ce6c1d08f0c7c193ffcfb4cba41beb425546ef
Oleksandr <vito.oleksandr@gmail.com>
2022-11-09 14:21:35 +0200

    changes http

--- src/service/getCitiesList.js
+++ src/service/getCitiesList.js
@@ -5,7 +5,7 @@ const getCitiesList = async (inputValue) => {
   };
 
   const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=10&lang=ua&appid=e9d5d0d4ac10e640808a8508992fd4ae`,
+    `https://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&limit=10&lang=ua&appid=e9d5d0d4ac10e640808a8508992fd4ae`,
     options
   )
     .then((response) => response.json())
